ea7025d6e122e5964661980ca599f35d80490c5a
Fouthress <132385479+Fouthress@users.noreply.github.com>
2023-10-29 22:43:41 +0700

    Add files via upload

--- javascript/home.js
+++ javascript/home.js
@@ -1,14 +1,24 @@
-let getTombol = document.getElementById("tombol");
+let mybutton = document.getElementById("btn-back-to-top");
 
-window.addEventListener("scroll", function(){
-    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        getTombol.style.display = "block";
-    } else{
-        getTombol.style.display = "none";
-    }
-});
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
 
-getTombol.addEventListener("click", function(){
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-})
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
